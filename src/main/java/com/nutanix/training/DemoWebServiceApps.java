62b19e3d44a4933e3ddc6c9743ff2c0eb25f5e0a
dimasm93 <software.dimas_m@icloud.com>
2024-01-09 13:50:37 +0700

    init project web

---
+++ src/main/java/com/nutanix/training/DemoWebServiceApps.java
@@ -0,0 +1,13 @@
+package com.nutanix.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebServiceApps {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebServiceApps.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutanix/training/DemoWebServiceAppsTests.java
@@ -0,0 +1,13 @@
+package com.nutanix.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebServiceAppsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
